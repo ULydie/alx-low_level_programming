7385c408097bb2cfa4bf660caac554381d1815d4
Lydie Umuhire <umuhirelydie2@gmail.com>
2023-06-13 05:50:04 -0700

    fix same betty styles

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,17 +11,21 @@
 
 char *create_array(unsigned int size, char c)
 {
+
 	unsigned int a;
 	char *s;
-	if(size == 0)
-		return (NULL);
-	s=malloc(size * sizeof(char));
-	if (s == NULL)
-		return (NULL);
-	for(a = 0; a < size; a++)
+	if( size == 0 )
+		return ( NULL );
+	s = malloc(size * sizeof(char));
+	if ( s == NULL )
+		return ( NULL );
+	for( a = 0; a < size; a++ )
 	{
+
 		s[a] = c;
+
 	}
 
 	return (s);
+
 }
