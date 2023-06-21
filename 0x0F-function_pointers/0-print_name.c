36d7e9c7694e6e0f0dd988d61991bc2c77e99760
ULydie <110610349+ULydie@users.noreply.github.com>
2023-06-21 17:09:18 +0200

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,15 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 
+/**
+ * print_name:functio that prints a name 
+ * @name: name of the person
+ *
+ * Return: Nothing.
+ */
 void print_name(char *name, void (*f)(char *))
 {
+	if(name == NULL || f == NULL)
+		return;
 	(*f)(name);
 }
